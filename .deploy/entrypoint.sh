940b3a902044235224f31462f44c65ed3e2ed9c0
ToXiiCxBusiness <73804003+ToXiiCxBusiness@users.noreply.github.com>
2020-11-03 18:07:31 -0500

    Update entrypoint.sh

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-echo "test"
 echo "🎬 entrypoint.sh"
 
 composer dump-autoload --no-interaction --no-dev --optimize
 
 echo "🎬 artisan commands"
+php artisan p:environment:setup
+php artisan p:environment:database
+
+php artisan migrate --seed
 
 php artisan cache:clear
 php artisan migrate --no-interaction --force
-
-echo "🎬 start supervisord"
-
-supervisord -c $LARAVEL_PATH/.deploy/config/supervisor.conf
