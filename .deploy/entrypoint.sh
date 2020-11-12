8c8a5e97008228fce7643101c025c33112763048
ToXiiCxBusiness <73804003+ToXiiCxBusiness@users.noreply.github.com>
2020-11-11 19:45:50 -0500

    Update entrypoint.sh

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
-echo "🎬 entrypoint.sh"
-
-composer dump-autoload --no-interaction --no-dev --optimize
-
-echo "🎬 artisan commands"
-php artisan p:environment:setup
-php artisan p:environment:database
-
-php artisan migrate --seed
-
-php artisan cache:clear
-php artisan migrate --no-interaction --force
-
 set -e
 
 if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
