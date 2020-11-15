39d563d870839e655538b2b90c35c15a0c1dd52d
ToXiiCxBusiness <73804003+ToXiiCxBusiness@users.noreply.github.com>
2020-11-15 18:54:56 -0500

    Update docker-entrypoint.sh

--- .deploy/docker-entrypoint.sh
+++ .deploy/docker-entrypoint.sh
@@ -33,7 +33,12 @@ if [ "$1" = "nginx" -o "$1" = "nginx-debug" ]; then
     fi
 fi
 
-echo "Start PHP-FPM service"
+echo "Start PHP-FPM Service"
 service php7.3-fpm start
+echo "Start Redis Service"
+service redis-server start
+echo "Run PHP Artisan"
+php artisan p:environment:setup --new-salt --author=business.toxiic@gmail.com --url=http://pterodactyl.toxiic.net --timezone=America/New_York --cache=redis --session=redis --queue=redis --redis-host=localhost --redis-pass= --redis-port=6379 --settings-ui=yes
+php artisan p:environment:database --host=srv-captain--www-db --port=3306 --database=pterodactyl_panel --username=pterodactyl --password=nul%I0nC2&G81jNfaP^O29RW3GT%C%
 
 exec "$@"
