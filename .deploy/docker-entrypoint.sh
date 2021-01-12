921b50b3ff66b5c89a79cbd5401b10c5469e99ff
ToXiiCxBusiness <73804003+ToXiiCxBusiness@users.noreply.github.com>
2021-01-12 10:13:31 -0500

    Update docker-entrypoint.sh

--- .deploy/docker-entrypoint.sh
+++ .deploy/docker-entrypoint.sh
@@ -32,6 +32,10 @@ if [ "$1" = "nginx" -o "$1" = "nginx-debug" ]; then
         echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
     fi
 fi
+echo "Start PHP-FPM Service"
+service php7.3-fpm start
+echo "Run PHP Artisan"
+ln persistent/.env .env --force
 
 php artisan key:generate --force --no-interaction
 php artisan p:environment:setup --new-salt --author=business.toxiic@gmail.com --url=http://pterodactyl.toxiic.net --timezone=America/New_York --cache=redis --session=redis --queue=redis --redis-host=srv-captain--redis --redis-pass=R6xMITCWLtn7eO8 --redis-port=6379 --settings-ui=yes --no-interaction
@@ -40,10 +44,5 @@ php artisan p:environment:database --host=srv-captain--mysql-db --port=3306 --da
 RUN chown -R www-data:www-data *
 
 RUN (crontab -l ; echo "* * * * * /usr/local/bin/php /srv/app/artisan schedule:run >> /dev/null 2>&1") | crontab
-
-echo "Start PHP-FPM Service"
-service php7.3-fpm start
-echo "Run PHP Artisan"
-ln persistent/.env .env --force
 
 exec "$@"
