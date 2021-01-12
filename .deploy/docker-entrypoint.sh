51bb571f83ea8e13280374713e9777e6a89120b0
ToXiiCxBusiness <73804003+ToXiiCxBusiness@users.noreply.github.com>
2021-01-12 11:18:39 -0500

    Update docker-entrypoint.sh

--- .deploy/docker-entrypoint.sh
+++ .deploy/docker-entrypoint.sh
@@ -37,10 +37,14 @@ echo "Start PHP-FPM Service"
 service php7.3-fpm start
 echo "Run PHP Artisan"
 
-#echo "More Artisan"
-php artisan key:generate --force --no-interaction
-php artisan p:environment:setup --new-salt --author=business.toxiic@gmail.com --url=http://pterodactyl.toxiic.net --timezone=America/New_York --cache=redis --session=redis --queue=redis --redis-host=srv-captain--redis --redis-pass=R6xMITCWLtn7eO8 --redis-port=6379 --settings-ui=yes --no-interaction
-php artisan p:environment:database --host=srv-captain--mysql-db --port=3306 --database=pterodactyl_panel --username=pterodactyl --password=kveBCfD6DQOBnco8 --no-interaction
+echo "Grabbing Latest .env"
+cd /var/www/pterodactyl
+wget https://raw.githubusercontent.com/ToXiiCxBusiness/pterodactyl/master/.deploy/.env
+
+#echo "More Artisan" Only used to make new config
+#php artisan key:generate --force --no-interaction
+#php artisan p:environment:setup --new-salt --author=business.toxiic@gmail.com --url=http://pterodactyl.toxiic.net --timezone=America/New_York --cache=redis --session=redis --queue=redis --redis-host=srv-captain--redis --redis-pass=R6xMITCWLtn7eO8 --redis-port=6379 --settings-ui=yes --no-interaction
+#php artisan p:environment:database --host=srv-captain--mysql-db --port=3306 --database=pterodactyl_panel --username=pterodactyl --password=kveBCfD6DQOBnco8 --no-interaction
 
 chown -R www-data:www-data *
 
