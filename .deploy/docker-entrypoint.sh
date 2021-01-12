5e325db6f2a8296346f53bfd7b65ddd6172ed579
ToXiiCxBusiness <73804003+ToXiiCxBusiness@users.noreply.github.com>
2021-01-12 10:19:10 -0500

    Update docker-entrypoint.sh

--- .deploy/docker-entrypoint.sh
+++ .deploy/docker-entrypoint.sh
@@ -28,22 +28,24 @@ if [ "$1" = "nginx" -o "$1" = "nginx-debug" ]; then
         done
 
         echo >&3 "$0: Configuration complete; ready for start up"
+        
+        echo "Start PHP-FPM Service"
+        service php7.3-fpm start
+        echo "Run PHP Artisan"
+        ln persistent/.env .env --force
+
+        echo "More Artisan"
+        php artisan key:generate --force --no-interaction
+        php artisan p:environment:setup --new-salt --author=business.toxiic@gmail.com --url=http://pterodactyl.toxiic.net --timezone=America/New_York --cache=redis --session=redis --queue=redis --redis-host=srv-captain--redis --redis-pass=R6xMITCWLtn7eO8 --redis-port=6379 --settings-ui=yes --no-interaction
+        php artisan p:environment:database --host=srv-captain--mysql-db --port=3306 --database=pterodactyl_panel --username=pterodactyl --password=kveBCfD6DQOBnco8 --no-interaction
+
+        RUN chown -R www-data:www-data *
+
+        RUN (crontab -l ; echo "* * * * * /usr/local/bin/php /srv/app/artisan schedule:run >> /dev/null 2>&1") | crontab
+
     else
         echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
     fi
 fi
-echo "Start PHP-FPM Service"
-service php7.3-fpm start
-echo "Run PHP Artisan"
-ln persistent/.env .env --force
-
-echo "More Artisan"
-php artisan key:generate --force --no-interaction
-php artisan p:environment:setup --new-salt --author=business.toxiic@gmail.com --url=http://pterodactyl.toxiic.net --timezone=America/New_York --cache=redis --session=redis --queue=redis --redis-host=srv-captain--redis --redis-pass=R6xMITCWLtn7eO8 --redis-port=6379 --settings-ui=yes --no-interaction
-php artisan p:environment:database --host=srv-captain--mysql-db --port=3306 --database=pterodactyl_panel --username=pterodactyl --password=kveBCfD6DQOBnco8 --no-interaction
-
-RUN chown -R www-data:www-data *
-
-RUN (crontab -l ; echo "* * * * * /usr/local/bin/php /srv/app/artisan schedule:run >> /dev/null 2>&1") | crontab
 
 exec "$@"
